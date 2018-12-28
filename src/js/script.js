5fe76a9726238cd138525b26d1541ecf3d5f51c9
Victor <victor.lerus@gmail.com>
2018-12-29 00:08:11 +0300

    Добавлен адаптив в header

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
+   let doc = document;
+   let toggleButton = doc.querySelector('.header__nav-toggle');
+   let menuList = doc.querySelector('.nav');
+   let menuListItem = doc.querySelectorAll('.nav__item');
+   
+   toggleButton.addEventListener('click', () => {
+      if(!menuList.classList.contains('active-nav')){
+         
+         menuList.classList.add('active-nav');
+         toggleButton.style.transform = 'rotate(90deg)'
+         for(i = 0; i < menuListItem.length; i++){
+            menuListItem[i].classList.add('active-nav-item');
+            menuListItem[i].style.opacity = '1';
+         }
+      }else{
+         
+         toggleButton.style.transform = '';
 
+         for(i = 0; i < menuListItem.length; i++){
+            
+            menuListItem[i].style.transition = 'all, 0.4s';
+            menuListItem[i].style.opacity = '0';
+            menuListItem[i].classList.remove('active-nav-item');
+         }
+         setTimeout( () =>{
+            menuList.classList.remove('active-nav')
+         }, 500);
+      }
+   });
+
+   window.addEventListener('resize',  () => {
+      console.log(1);
+      if(doc.body.clientWidth >= 610){
+         menuList.classList.remove('active-nav');
+         toggleButton.style.transform = '';
+         for(i = 0; i < menuListItem.length; i++){
+            menuListItem[i].classList.remove('active-nav-item');
+            menuListItem[i].style.opacity = '1';
+         }
+      }
+   });
+   
    
 });
 
